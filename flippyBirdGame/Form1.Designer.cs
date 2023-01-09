3a05958a277296115f9051079de97545b4aea4e7
Isanjalee Silva <silvaaihn.19@uom.lk>
2023-01-09 23:58:24 +0530

    Add project files.

---
+++ flippyBirdGame/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace flippyBirdGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Peru;
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Location = new System.Drawing.Point(12, 386);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 52);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Peru;
+            this.label1.Font = new System.Drawing.Font("SansSerifExbFLF", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(395, 386);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 52);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.GameTimeTikEvent);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::flippyBirdGame.Properties.Resources.heart;
+            this.pictureBox7.Location = new System.Drawing.Point(135, 2);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(59, 47);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 2;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::flippyBirdGame.Properties.Resources.heart;
+            this.pictureBox6.Location = new System.Drawing.Point(70, 2);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(59, 47);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 2;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::flippyBirdGame.Properties.Resources.heart;
+            this.pictureBox5.Location = new System.Drawing.Point(5, 3);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(59, 47);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::flippyBirdGame.Properties.Resources.kindpng_1153893;
+            this.pictureBox4.Location = new System.Drawing.Point(-4, 351);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(666, 134);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::flippyBirdGame.Properties.Resources.down;
+            this.pictureBox2.Location = new System.Drawing.Point(402, -81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 177);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::flippyBirdGame.Properties.Resources.PngItem_1842899;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::flippyBirdGame.Properties.Resources.up;
+            this.pictureBox3.Location = new System.Drawing.Point(422, 231);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 240);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("SansSerifBldFLF-Italic", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(557, 130);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Visible = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::flippyBirdGame.Properties.Resources.heart;
+            this.pictureBox8.Location = new System.Drawing.Point(525, 134);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(59, 47);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 2;
+            this.pictureBox8.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(609, 450);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDownEvent);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUpEvent);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox8;
+    }
+}
+
---
+++ flippyBirdGame/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flippyBirdGame
+{
+    public partial class Form1 : Form
+    {
+        int gravity = 15;
+        int pipeSpeed = 8;
+        int gameScore = 0;
+        int lifeCount = 3;
+        int lifeFreeTime = 0;
+
+        System.Media.SoundPlayer bye = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer die = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer getm = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer getheart = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer hit = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer playing = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer swoosh = new System.Media.SoundPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bye.SoundLocation = "bye.wav";
+            die.SoundLocation = "die.wav";
+            getm.SoundLocation = "get.wav";
+            getheart.SoundLocation = "getheart.wav";
+            hit.SoundLocation = "hit.wav";
+            playing.SoundLocation = "playing.wav";
+            swoosh.SoundLocation = "swoosh.wav";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimeTikEvent(object sender, EventArgs e)
+        {
+            
+            birdFadingEffect();
+            lifeFreeTime += 20;
+            pictureBox1.Top += gravity;
+            pictureBox2.Left -= pipeSpeed;
+            pictureBox3.Left -= pipeSpeed;
+            pictureBox8.Left-= pipeSpeed;
+
+            
+
+            if (pictureBox2.Left < -150)
+            {
+                pictureBox2.Left = 750;
+                gameScore++;
+                getheart.Play();
+                
+            }
+            if (pictureBox3.Left < -90)
+            { 
+                pictureBox3.Left = 850;
+                gameScore++;
+                getheart.Play();
+
+            }
+
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox2.Bounds) || pictureBox1.Bounds.IntersectsWith(pictureBox3.Bounds) || pictureBox1.Bounds.IntersectsWith(pictureBox4.Bounds))
+            {
+                endGame();
+            }
+
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox8.Bounds) && lifeCount <3)
+            {
+                extraLifeFunction();
+               
+            }
+
+            if(pictureBox8.Left < -10)
+            {
+                pictureBox8.Left = 10000;
+            }
+
+            if (lifeCount == 1)
+            {
+                pictureBox5.Visible = true;
+            }
+            if (lifeCount == 2)
+            {
+                pictureBox6.Visible = true;
+            }
+            if (lifeCount == 3)
+            {
+                pictureBox7.Visible = true;
+            }
+
+            label1.Text = "Score:" + gameScore;
+        }
+
+        private void keyUpEvent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) {
+                gravity = 15;
+            }
+        }
+
+        private void keyDownEvent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -15;
+            }
+        }
+
+        private void endGame()
+        {
+            if (lifeCount == 0 && lifeFreeTime > 1000)
+            {
+                timer1.Stop();
+                label2.Text = "Game Over!!!\nYour Score: " + gameScore;
+                label2.Visible = true;
+
+
+                swoosh.Play();
+                //end.Play();
+            }
+            else
+            {
+                switch (lifeCount)
+                {
+                    case 3:
+                        if (lifeFreeTime > 2500) {
+                            pictureBox7.Visible = false;
+                            lifeCount--; 
+                            lifeFreeTime = 0;
+                            hit.Play();
+                        }
+                        break;
+                    case 2:
+                        if (lifeFreeTime > 2500)
+                        {
+                            pictureBox6.Visible = false;
+                            lifeCount--;
+                            lifeFreeTime = 0;
+                            hit.Play();
+                        }
+                        break;
+                    case 1:
+                        if (lifeFreeTime > 2500)
+                        {
+                            pictureBox5.Visible = false;
+                            lifeCount--;
+                            lifeFreeTime = 0;
+                            hit.Play();
+                        }; 
+                        break;
+                }
+            }
+
+        }
+
+        private void birdFadingEffect()
+        {
+            if (pictureBox1.Visible == true && lifeFreeTime < 1000)
+            {
+                pictureBox1.Visible = false;
+            } else if (pictureBox1.Visible == false && lifeFreeTime < 1000)
+            {
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void extraLifeFunction()
+        {
+            if(lifeCount < 3)
+            {
+                lifeCount++;
+                pictureBox8.Left = 10000;
+                getheart.Play();
+            }
+            
+        }
+    
+    }
+}
